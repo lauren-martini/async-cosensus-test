f37452e8434de9a505d1b4d067d15c4fa4200b02
Lauren Martini <38046893+lauren-martini@users.noreply.github.com>
2022-08-20 15:14:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.onload = function(){
   var requestAnimation = function( obj ){
     obj.style.animation = 'none';
     window.requestAnimationFrame(function(){
-      obj.style.animation = 'color_touch 5s';
+      obj.style.animation = 'color_touch 15s';
     });
   }
 };
